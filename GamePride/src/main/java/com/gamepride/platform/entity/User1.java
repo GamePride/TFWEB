f4b6a9d18e381438374c367dfc706ab2066dc3d8
Cezzard99 <u201615014@upc.edu.pe>
2019-09-27 15:01:28 -0500

    user update

--- GamePride/src/main/java/com/gamepride/platform/entity/User1.java
+++ GamePride/src/main/java/com/gamepride/platform/entity/User1.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -25,6 +26,9 @@ public class User1 implements Serializable {
 	@Column(name="password",nullable=false)
 	private String password;
 
+	@OneToOne
+	
+	
 	public int getId() {
 		return id;
 	}
