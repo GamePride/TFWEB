6b80b35e0222f1cab3649e7f41f94f0aca09c814
Cezzard99 <u201615014@upc.edu.pe>
2019-09-27 10:22:58 -0500

    event update

--- GamePride/src/main/java/com/gamepride/platform/entity/Event.java
+++ GamePride/src/main/java/com/gamepride/platform/entity/Event.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="payment")
+@Table(name="event")
 public class Event implements Serializable{
 
 	private static final long serialVersionUID = 1L;
