b79278d4fce93b12bd47dab570276674bba5aaf8
Juan Alonso Cardenas <u201523692@upc.edu.pe>
2019-09-27 11:49:20 -0500

    up

--- GamePride/src/main/java/com/gamepride/platform/entity/Person.java
+++ GamePride/src/main/java/com/gamepride/platform/entity/Person.java
@@ -28,4 +28,25 @@ public class Person implements Serializable {
 	@Column(name="account_number",nullable=false,length=30)
 	private int accountNumber;
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+	
+	
 }
