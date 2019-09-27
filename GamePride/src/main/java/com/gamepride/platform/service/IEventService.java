f25df7978fcca9bcbe9a19bc58ef9eefb56e3b9f
carlos07071 <u201617072@upc.edu.pe>
2019-09-27 18:03:38 -0500

    EServirce

---
+++ GamePride/src/main/java/com/gamepride/platform/service/IEventService.java
@@ -0,0 +1,11 @@
+package com.gamepride.platform.service;
+
+import java.util.List;
+
+import com.gamepride.platform.entity.Event;
+
+public interface IEventService {
+	public void insertar(Event event);
+	
+	public List<Event>listar();
+}
